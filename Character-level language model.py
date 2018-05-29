7dbc4cd48b89e19c043d2c0a41e57ed863088f95
shiva.m <shiva.m@csd.com>
2018-05-29 17:36:52 +0530

    Added all files

---
+++ Character-level language model.py
@@ -0,0 +1,2 @@
+#http://karpathy.github.io/2015/05/21/rnn-effectiveness/
+#https://gist.github.com/karpathy/d4dee566867f8291f086
---
+++ Multi Layer Perceptron (Feed Forward Network).py
@@ -0,0 +1,49 @@
+#https://iamtrask.github.io/2015/07/12/basic-python-network/ <need to implement>
+#https://github.com/miloharper/multi-layer-neural-network/blob/master/main.py
+
+from numpy import exp, array, random, dot
+
+class NeuralNetwork():
+    def __init__(self):
+        random.seed(1)
+
+        self.synaptic_weights = 2 * random.random((3,1)) - 1
+        # syn0 = 2*np.random.random((3,4)) - 1
+        # syn1 = 2*np.random.random((4,1)) - 1
+    
+    def __sigmoid(self, x):
+        return 1 / (1 + exp(-x))
+    
+    def __sigmoid_derivative(self, x):
+        return x * (1 - x)
+    
+    def think(self, inputs):
+        return self.__sigmoid(dot(inputs, self.synaptic_weights))
+    
+    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+        for iteration in range(number_of_training_iterations):
+            output = self.think(training_set_inputs)
+            error = training_set_outputs - output
+            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output)) #why product of MM(inputs, error*sigmoid_derivative(output))
+            self.synaptic_weights += adjustment
+    
+if __name__ == "__main__":
+
+    neural_network = NeuralNetwork()
+
+    print("Random starting synaptic weights: ")
+    print(neural_network.synaptic_weights)
+
+    training_set_inputs = array([[0,0,1],[1,1,1],[1,0,1],[0,1,1]])
+    training_set_outputs = array([[0,1,1,0]]).T
+
+    neural_network.train(training_set_inputs,training_set_outputs,10000)
+    print("New synaptic weights after training: ")
+    print(neural_network.synaptic_weights)
+
+    print("Considering new situation [1,0,0] -> ?: ")
+    print(neural_network.think(array([1,0,0])))
+
+
+            
+
