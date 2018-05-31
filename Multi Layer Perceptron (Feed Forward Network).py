a636cdc6c38f877cd0e3111d2bab0d719be71071
shiva.m <shiva.m@csd.com>
2018-05-31 21:05:11 +0530

    Added files 31 May 2018

--- Multi Layer Perceptron (Feed Forward Network).py
+++ Multi Layer Perceptron (Feed Forward Network).py
@@ -44,6 +44,30 @@ if __name__ == "__main__":
     print("Considering new situation [1,0,0] -> ?: ")
     print(neural_network.think(array([1,0,0])))
 
+## 2-Layer feed forward neural networks
+# l0 = X
+# l1 = nonlin(np.dot(l0,syn0))
+# l2 = nonlin(np.dot(l1,syn1))
+ 
+# # how much did we miss the target value?
+# l2_error = y - l2
 
-            
+# if (j% 10000) == 0:
+#     print "Error:" + str(np.mean(np.abs(l2_error)))
 
+# # in what direction is the target value?
+# # were we really sure? if so, don't change too much.
+
+# l2_delta = l2_error*nonlin(l2,deriv=True)
+
+# # how much did each l1 value contribute to the l2 error (according to the weights)?
+
+# l1_error = l2_delta.dot(syn1.T)
+
+# # in what direction is the target l1?
+# # were we really sure? if so, don't change too much.
+
+# l1_delta = l1_error * nonlin(l1,deriv=True)
+
+# syn1 += l1.T.dot(l2_delta)
+# syn0 += l0.T.dot(l1_delta)
